bc3ff4f9b58743e4d99d87fc9aace659fa08f1a2
Liyuanlong <959769993@qq.com>
2026-01-17 13:13:28 +0800

    rm backup files

--- tools/akshare/shareholder_change_record.py
+++ tools/akshare/shareholder_change_record.py
@@ -17,20 +17,17 @@ def convert_nan_to_none(value):
         return None
     return value
 
+
 def convert_tuple_nan_to_none(data_tuple):
     """转换元组中的NaN值为None"""
     return tuple(convert_nan_to_none(item) for item in data_tuple)
 
 
-def insert_dataframe_to_mysql(df, table_name, table_columns):
+def insert_dataframe_to_mysql(fetch_date, need_total, df, table_name, table_columns):
     """
     将DataFrame数据逐行插入到MySQL数据库
     """
 
-    current_date = date.today()
-    days_ago = current_date - timedelta(days=1)
-    fetch_date = days_ago.strftime("%Y-%m-%d")
-    print("==============DAILY BEGIN: {fetch_date}=====================")
     try:
         # 连接数据库
         mydb = pymysql.connect(
@@ -58,8 +55,8 @@ def insert_dataframe_to_mysql(df, table_name, table_columns):
             try:
                 # 将行数据转换为元组
                 values = tuple(convert_tuple_nan_to_none(row))
-                if fetch_date is not None and values[-1].strftime("%Y-%m-%d") != fetch_date:
-                   continue
+                if need_total == False and fetch_date is not None and values[-1].strftime("%Y-%m-%d") != fetch_date:
+                    continue
 
                 mycursor.execute(sql, values)
                 success_count += 1
@@ -85,9 +82,16 @@ def insert_dataframe_to_mysql(df, table_name, table_columns):
             mycursor.close()
         if 'mydb' in locals():
             mydb.close()
-    print("==============DAILY END: {fetch_date}=====================")
 
 
 if __name__ == '__main__':
-    df = stock_ggcg_em_df = ak.stock_ggcg_em(symbol="全部")   # symbol="全部"; choice of {"全部", "股东增持", "股东减持"}  # 读取数据
-    insert_dataframe_to_mysql(df=df, table_name='shareholder_change_record',table_columns='stock_code,stock_name,latest_price,change_percent,shareholder_name,change_type,change_amount,change_total_ratio,change_circulation_ratio,after_total_holdings,after_total_ratio,after_circulation_holdings,after_circulation_ratio,change_start_date,change_end_date,announcement_date')
+    current_date = date.today()
+    days_ago = current_date - timedelta(days=1)
+    fetch_date = days_ago.strftime("%Y-%m-%d")
+    table_cols = 'stock_code,stock_name,latest_price,change_percent,shareholder_name,change_type,change_amount,change_total_ratio,change_circulation_ratio,after_total_holdings,after_total_ratio,after_circulation_holdings,after_circulation_ratio,change_start_date,change_end_date,announcement_date'
+
+    print("==============DAILY BEGIN: {fetch_date}=====================")
+    df = stock_ggcg_em_df = ak.stock_ggcg_em(symbol="全部")  # symbol="全部"; choice of {"全部", "股东增持", "股东减持"}  # 读取数据
+    insert_dataframe_to_mysql(fetch_date=fetch_date, need_total=False, df=df, table_name='shareholder_change_record',
+                              table_columns=table_cols)
+    print("==============DAILY END: {fetch_date}=====================")
